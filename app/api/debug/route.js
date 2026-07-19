23ec988520c49a61c8ffee2f937eda9812e81a3a
Trans Party <comeandsee@gmail.com>
2026-07-19 09:07:26 +0100

    Update debug endpoint to check Upstash vars

--- app/api/debug/route.js
+++ app/api/debug/route.js
@@ -2,10 +2,9 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
+    UPSTASH_REDIS_REST_URL: !!process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: !!process.env.UPSTASH_REDIS_REST_TOKEN,
+    JWT_SECRET: !!process.env.JWT_SECRET,
     DATABASE_URL: !!process.env.DATABASE_URL,
-    POSTGRES_URL: !!process.env.POSTGRES_URL,
-    DATABASE_URL_UNPOOLED: !!process.env.DATABASE_URL_UNPOOLED,
-    PGHOST: !!process.env.PGHOST,
-    PGDATABASE: !!process.env.PGDATABASE,
   });
 }
